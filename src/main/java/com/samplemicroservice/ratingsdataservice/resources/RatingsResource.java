42309b10da7b0a969532ba2ef75655a0ffe8d844
Moumita Ray <moumitacloud08@gmail.com>
2020-10-28 16:21:10 +0530

    Making a call to rating api

--- src/main/java/com/samplemicroservice/ratingsdataservice/resources/RatingsResource.java
+++ src/main/java/com/samplemicroservice/ratingsdataservice/resources/RatingsResource.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsResource {
@@ -12,5 +15,13 @@ public class RatingsResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId){
         return new Rating(movieId,4);
+    }
+    @RequestMapping("users/{userId}")
+    public List<Rating> getRatings(@PathVariable("userId") String userId){
+        List<Rating> ratings =  Arrays.asList(
+                new Rating("1234",4),
+                new Rating("5643",5)
+        );
+        return ratings;
     }
 }
